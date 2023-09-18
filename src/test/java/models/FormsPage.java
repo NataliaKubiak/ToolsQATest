19b0866bdade7884616d72704b731129188af8d8
Natalia Kubiak <nataliia.kubiak@gmail.com>
2023-09-18 11:43:03 +0200

    add random data generation methods (variant 2)

--- src/test/java/models/FormsPage.java
+++ src/test/java/models/FormsPage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 import tests.testData.Gender;
 
+import java.util.Calendar;
 import java.util.List;
 import java.util.Random;
 
@@ -82,8 +83,10 @@ public class FormsPage extends BasePage {
         return this;
     }
 
-    public FormsPage chooseDateOfBirthRandom() {
+    //date picker - variant 1
+    public FormsPage chooseDateOfBirthRandomClickOnPicker() {
         dateOfBirthInput.click();
+
         Random randomNumber = new Random();
         String month = randomNumber.nextInt(13) + "";
         String year = randomNumber.nextInt(1900, 2023) + "";
@@ -102,6 +105,42 @@ public class FormsPage extends BasePage {
         getDriver().findElement(By
                 .xpath("//div[@class='react-datepicker__week'][" + randomWeekIndex +
                         "]/descendant::div[" + randomDayIndex + "]")).click();
+
+        return this;
+    }
+
+    //date picker - variant 2
+    public FormsPage chooseDateOfBirthRandomInChosenMonth() {
+        dateOfBirthInput.click();
+
+        Random randomNumber = new Random();
+        int month = randomNumber.nextInt(13);
+        int year = randomNumber.nextInt(1900, 2024);
+
+        Select selectMonthMenu = new Select(monthMenu);
+        selectMonthMenu.selectByValue(month+ "");
+
+        Select selectYearMenu = new Select(yearMenu);
+        selectYearMenu.selectByValue(year + "");
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+
+        int daysInMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        int randomPickedDay = randomNumber.nextInt(1, daysInMonth + 1);
+        String daysInMontIndex;
+
+        if (randomPickedDay < 10) {
+            daysInMontIndex = "00" + randomPickedDay;
+        } else {
+            daysInMontIndex = "0" + randomPickedDay;
+        }
+
+        getDriver().findElement(By
+                .xpath("//div[@class='react-datepicker__day react-datepicker__day--" +
+                        daysInMontIndex + "']")).click();
 
         return this;
     }
